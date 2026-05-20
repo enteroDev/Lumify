2ff90fc77a3837429dda8a78c9b5905e9562766a
ent.chris <christian.riepl@lbs4.salzburg.at>
2026-05-20 15:16:38 +0200

    [FIX] Corrected nullable property in WorkspaceMemberResponse

--- lumify.api/Models/DTO/Responses/WorkspaceMemberResponse.cs
+++ lumify.api/Models/DTO/Responses/WorkspaceMemberResponse.cs
@@ -5,7 +5,7 @@ namespace lumify.api.Models.DTO.Responses
     public class WorkspaceMemberResponse
     {
         public string UserID { get; set; } = "";
-        public string AvatarUrl { get; set; } = "";
+        public string? AvatarUrl { get; set; }
         public string DisplayName { get; set; } = "";
         public string Username { get; set; } = "";
         public string Email { get; set; } = "";
