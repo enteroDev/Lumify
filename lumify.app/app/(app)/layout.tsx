578853a2d8ab7e62cff36a153d8c1022f994df80
ent.chris <christian.riepl@lbs4.salzburg.at>
2026-05-22 08:17:51 +0200

    [STRUCT] Prepared Frontend layouts and groups for this app

--- lumify.app/app/(app)/layout.tsx
+++ lumify.app/app/(app)/layout.tsx
@@ -1,45 +1,19 @@
 // Components
 import Header from "@/components/Header/Header";
-import Drawer from "@/components/Drawer/Drawer";
-import Friends from "@/components/FriendsOverlay/FriendsOverlay";
-import PresenceBridge from "@/services/utils/presenceBridge";
 
 // Providers
 import MusicProvider from "@/components/_Audio/MusicProvider";
-import ToastProvider from "@/components/Toast/ToastProvider";
-import TooltipProvider from "@/components/Tooltip/TooltipProvider";
-import AlertProvider from "@/components/AlertModal/AlertProvider";
-import AccountModalProvider from "@/components/AccountModal/AccountModalProvider";
-import ModalProvider from "@/components/Modal/ModalProvider";
-import SpaceProvider from "@/components/_Space/SpaceProvider";
 
 
 
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ToastProvider>
-      <TooltipProvider>
-        <AlertProvider>
-          <MusicProvider>
-            <ModalProvider>
-              <AccountModalProvider>
-
-                <PresenceBridge>
-                  <SpaceProvider>
-                    <Header />
-                    <Drawer />
-                    <Friends />
-                    <main>{children}</main>
-                  </SpaceProvider>
-                </PresenceBridge>
-
-
-              </AccountModalProvider>
-            </ModalProvider>
-          </MusicProvider>
-        </AlertProvider>
-      </TooltipProvider>
-    </ToastProvider>
+    <>
+      <MusicProvider>
+        <Header />
+        <main>{children}</main>
+      </MusicProvider>
+    </>
   );
 }
--- lumify.app/app/(auth)/layout.tsx
+++ lumify.app/app/(auth)/layout.tsx
@@ -1,8 +1,4 @@
 // Provider
-import ToastProvider from "@/components/Toast/ToastProvider";
-import TooltipProvider from "@/components/Tooltip/TooltipProvider";
-import AlertProvider from "@/components/AlertModal/AlertProvider";
-import AccountModalProvider from "@/components/AccountModal/AccountModalProvider";
 import MusicProvider from "@/components/_Audio/MusicProvider";
 
 // Components
@@ -12,17 +8,9 @@ import Header from "@/components/Header/Header";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
-        <ToastProvider>
-            <TooltipProvider>
-                <AlertProvider>
-                    <AccountModalProvider>
-                        <MusicProvider>
-                            <Header />
-                            <main>{children}</main>
-                        </MusicProvider>
-                    </AccountModalProvider>
-                </AlertProvider>
-            </TooltipProvider>
-        </ToastProvider>
+        <MusicProvider>
+            <Header />
+            <main>{children}</main>
+        </MusicProvider>
     );
 }
