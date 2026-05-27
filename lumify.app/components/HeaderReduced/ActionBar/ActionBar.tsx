0a635c3bdebd5fe4511c615141075b9f6105dce1
ent.chris <christian.riepl@lbs4.salzburg.at>
2026-05-27 16:28:46 +0200

    [FIX / LOG] Removed MusicProvider dependencies in HeaderReduced. (Auth does not need that) Updated Todos.md

--- lumify.app/components/HeaderReduced/ActionBar/ActionBar.tsx
+++ lumify.app/components/HeaderReduced/ActionBar/ActionBar.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useMusic } from "../../_Audio/MusicProvider";
-
 import SoundOn from "../../../app/src/svg/soundOn.svg";
 import SoundOff from "../../../app/src/svg/soundOff.svg";
 import Sun from "../../../app/src/svg/sun.svg";
@@ -23,16 +21,13 @@ export const c = {
 
 
 export default function ActionBar() {
-    const { isEnabled, toggle } = useMusic();
+
 
     return (
         <div className={c.container}>
             <div className={c.actionPill}>
-                <button className={c.actionButton} onClick={toggle} type="button" >
-                    {isEnabled
-                        ? <SoundOn className={c.iconSound} width={20} height={20} />
-                        : <SoundOff className={c.iconSound} width={20} height={20} />
-                    }
+                <button className={c.actionButton} type="button" >
+                    <SoundOn className={c.iconSound} width={20} height={20} />
                 </button>
                 <button className={c.actionButton}>
                     <Sun className={c.iconTheme} width={20} height={20} />
--- lumify.app/docs/todos.md
+++ lumify.app/docs/todos.md
@@ -2,10 +2,11 @@
 
 ## Fixes
 [] AccountModal: Remove Workspace entry in Sidebar of AccountModal (Not needed probably)
-[] FriendsPanel: Add notification count in TabButton
+[] FriendsPanel: Check LiveSync when getting added by another user. Needs a reload currently
 
 ## Implementations
-
+[] FriendsPanel: Add notification count in TabButton
+[] FriendsPanel: If no related user yet, display some other user, so the field is not empty. Or add a placeholder with text "Search for a user"
 
 
 # AUTH
